f96d4dfbf95b4f004c0ce925c9e2212aa69d91b6
sinnosuke2701 <ehgml1111@naver.com>
2024-09-04 17:33:38 +0900

    shinchan

---
+++ shinchan/src/main/java/com/shinnosuke/ShinchanApplication.java
@@ -0,0 +1,13 @@
+package com.shinnosuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinchanApplication.class, args);
+	}
+
+}
---
+++ shinchan/src/test/java/com/shinnosuke/ShinchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinnosuke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
