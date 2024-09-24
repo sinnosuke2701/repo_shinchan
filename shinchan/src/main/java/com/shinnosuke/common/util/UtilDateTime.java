ba1fad52c0eb4cc9a4f3ab1c1f9e0be064cefd2e
shinnosuke <shinnosuke@localhost>
2024-09-24 17:29:00 +0900

    util

---
+++ shinchan/src/main/java/com/shinnosuke/common/util/UtilDateTime.java
@@ -0,0 +1,13 @@
+package com.shinnosuke.common.util;
+
+public class UtilDateTime {
+	
+	public static String add00TimeString(String date) {
+		return date + " 00:00:00";
+	}
+
+	public static String add59TimeString(String date) {
+		return date + " 23:59:59";
+	}
+	
+}
