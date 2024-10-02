303d448938814212333b6a3dade13c58197f502e
shinnosuke <shinnosuke@192.168.0.24>
2024-10-02 15:52:22 +0900

    constants

---
+++ shinchan/src/main/java/com/shinnosuke/common/constants/Constants.java
@@ -0,0 +1,16 @@
+package com.shinnosuke.common.constants;
+
+public class Constants {
+	
+	public static final int ROW_NUM_TO_SHOW = 5;
+	public static final int PAGE_NUM_TO_SHOW = 5;
+	
+	
+	//세션
+	public static final int SESSION_MINUTE_XDM = 30;
+	
+	
+	//interceptor
+	public static final String URL_LOGINFORM = "/xdm/v1/infra/member/signinXdmForm";
+	
+}
