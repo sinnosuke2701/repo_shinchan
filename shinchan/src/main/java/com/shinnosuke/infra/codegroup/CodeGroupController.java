0abfb09c513990e2e56fb5b049ca48dfd119c938
shinnosuke <shinnosuke@localhost>
2024-11-06 09:13:06 +0900

    이동현

--- shinchan/src/main/java/com/shinnosuke/infra/codegroup/CodeGroupController.java
+++ shinchan/src/main/java/com/shinnosuke/infra/codegroup/CodeGroupController.java
@@ -1,5 +1,11 @@
 package com.shinnosuke.infra.codegroup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shinnosuke.common.util.UtilDateTime;
 
 @Controller
@@ -17,7 +25,7 @@ public class CodeGroupController {
 	CodeGroupService CodeGroupService;
 
 	@RequestMapping(value = "/xdm/v1/infra/codegroup/codeGroupXdmList")
-	public String codeGroupXdmList(Model model , @ModelAttribute("vo") CodeGroupVo codeGroupVo) {
+	public String codeGroupXdmList(Model model , @ModelAttribute("vo") CodeGroupVo codeGroupVo) throws IOException {
 		
 //		codeGroupVo.setSh_DateStart(codeGroupVo.getSh_DateStart()+ " 00:00:00");
 //		codeGroupVo.setSh_DateEnd(codeGroupVo.getSh_DateEnd()+ " 23:59:59");
@@ -34,7 +42,73 @@ public class CodeGroupController {
 		//모든객체를 html로 넘길때 model을 사용 / codegroups의 내용을 list 에 담아서 리턴으로 보냄 .
 //		model.addAttribute("list",CodeGroupService.selectList()); //내용을 담을게 없으면 축약형.
 		
-
+//	        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getCovidDagnsRgntProdExprtStusInq"); /*URL*/
+//	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=s3EoCZd6XTrLGuF1DyqzsTVzxBYJUAd8Rv8rDDBxPZQJU1gY73d00DuEE6tFGhcHN6nnesTAP%2Fj2n5SBd9zhDA%3D%3D"); /*Service Key*/
+//	        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+//	        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); /*한 페이지 결과 수*/
+//	        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*응답데이터 형식(xml/json) default : xml*/
+//	        urlBuilder.append("&" + URLEncoder.encode("YYYY","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*년도*/
+//	        urlBuilder.append("&" + URLEncoder.encode("MM","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*실적월*/
+//	        URL url = new URL(urlBuilder.toString());
+//	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//	        conn.setRequestMethod("GET");
+//	        conn.setRequestProperty("Content-type", "application/json");
+//	        System.out.println("Response code: " + conn.getResponseCode());
+//	        BufferedReader rd;
+//	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+//	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//	        } else {
+//	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+//	        }
+//	        StringBuilder sb = new StringBuilder();
+//	        String line;
+//	        while ((line = rd.readLine()) != null) {
+//	            sb.append(line);
+//	        }
+//	        rd.close();
+//	        conn.disconnect();
+//	        System.out.println(sb.toString());
+//	        
+//	        ObjectMapper objectMapper = new ObjectMapper();
+//			JsonNode node = objectMapper.readTree(sb.toString());
+//			
+//			System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
+//			System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
+//			System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(0).get("KIT_PROD_QTY").asText());
+			
+		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getMmCovidDagnsRgntExprtStusInq"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=s3EoCZd6XTrLGuF1DyqzsTVzxBYJUAd8Rv8rDDBxPZQJU1gY73d00DuEE6tFGhcHN6nnesTAP%2Fj2n5SBd9zhDA%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*응답데이터 형식(xml/json) default : xml*/
+        urlBuilder.append("&" + URLEncoder.encode("YYYY","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*년도*/
+        urlBuilder.append("&" + URLEncoder.encode("MM","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*실적월*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode node = objectMapper.readTree(sb.toString());
+		
+		System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
+		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
+		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("KIT_EXPRT_QTY").asText());
 		return "/xdm/v1/infra/codegroup/codeGroupXdmList";
 
 	}
